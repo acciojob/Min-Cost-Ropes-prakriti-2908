9025b610537f48a2699f6550ab730da3bad198c2
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-24 23:21:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	// Sort the array
+	arr.sort((a,b) => a-b);
+
+	while(arr.length > 1){
+		// Add the first two elements
+		let sum = arr[0] + arr[1];
+		
+		// Remove the first two elements
+		arr.shift();
+		arr.shift();
+		
+		// Insert the sum back into the array
+		arr.unshift(sum);
+
+		// Sort the array again
+		arr.sort((a,b) => a-b);
+	}
+	let minCost = arr[0];
+	return minCost;
 }
 
 module.exports=mincost;
