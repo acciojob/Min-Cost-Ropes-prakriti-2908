8c492c4881bdb9e52166342bfde2419cb807b29b
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-24 23:28:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ function mincost(arr)
 		let sum = arr[0] + arr[1];
 		
 		// Remove the first two elements
-		arr.shift();
-		arr.shift();
+		arr.splice(0, 2);
 		
 		// Insert the sum back into the array
-		arr.unshift(sum);
+		arr.push(sum);
 
 		// Sort the array again
 		arr.sort((a,b) => a-b);
@@ -20,5 +19,4 @@ function mincost(arr)
 	let minCost = arr[0];
 	return minCost;
 }
-
 module.exports=mincost;
